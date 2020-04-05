a5330c58fd013e1358ebbac3d9c0dcd7e11fe25e
이광기 <gwanggi.lee@gmail.com>
2020-04-05 20:47:14 +0900

    Init project

---
+++ src/test/kotlin/com/jayden/community/CommunityApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jayden.community
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
